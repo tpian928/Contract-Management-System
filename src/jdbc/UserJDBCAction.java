723077d5503a3079956a487c04bfcd211ce2ec3d
王怡飞 <TPIAN@wangyifeideMacBook-Air.local>
2015-05-31 13:38:34 +0800

    start

---
+++ src/jdbc/UserJDBCAction.java
@@ -0,0 +1,27 @@
+package jdbc;
+
+
+public class UserJDBCAction {
+
+//	/**
+//	 * 添加用户
+//	 * @param name 用户名
+//	 * @param password 密码
+//	 * @return 是否成功添加到User表
+//	 */
+//	public static boolean addUser(String name,String password) {
+//		
+//	}
+//	
+//
+//	/**
+//	 * 登录时进行判断，是不是我的用户
+//	 * @param name 用户名
+//	 * @param password 用户密码
+//	 * @return user表的id,如果没有这个用户则返回空字符串
+//	 */
+//	public static String isMyUser(String name,String password) {
+//		
+//	}
+	
+}
