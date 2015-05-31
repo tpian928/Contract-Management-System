ddefedb172830f5f8ec68ed150ccb8dc1b19c0da
王怡飞 <TPIAN@wangyifeideMacBook-Air.local>
2015-05-31 14:10:48 +0800

    完成注册登录的后端

--- src/sever/LoginServlet.java
+++ src/sever/LoginServlet.java
@@ -3,63 +3,52 @@ package sever;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONStringer;
 
 import jdbc.UserJDBCAction;
 
 
-public class RegisterServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public RegisterServlet() {
+
+    public LoginServlet() {
         super();
-        
+
     }
 
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 	}
 
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		request.setCharacterEncoding("UTF-8");
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
 		String idString = UserJDBCAction.isMyUser(name, password);
-		
-		//result
-		//0-成功注册
 		JSONObject jsonObject = new JSONObject(); 
-		
-		if (idString.length()==0) {
-			try {
-				UserJDBCAction.addUser(name, password);
-				idString = UserJDBCAction.isMyUser(name, password);
+		try {
+			if (idString.length()!=0) {
+				//返回idString
 				jsonObject.put("id", idString);
-			} catch (Exception e) {
-				try {
-					jsonObject.put("id", "-1");
-				} catch (JSONException e1) {
-					e1.printStackTrace();
-				}			
 			}
-		}
-		else{
+			else {
+				jsonObject.put("id", "");
+			}
+		} catch (Exception e) {
 			try {
-				jsonObject.put("id", "-2");
-			} catch (JSONException e) {
-				e.printStackTrace();
+				jsonObject.put("id", "");
+			} catch (JSONException e1) {
+				e1.printStackTrace();
 			}
 		}
-		
+
 		//返回
 	    response.getOutputStream().write(jsonObject.toString().getBytes("UTF-8"));  
 	    response.setContentType("text/json; charset=UTF-8"); 
