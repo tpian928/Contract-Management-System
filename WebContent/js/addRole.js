702aff5d50ffb01581ad53afbcaea059b8ae93a3
王怡飞 <TPIAN@wangyifeideMacBook-Air.local>
2015-06-18 18:32:24 +0800

    add js搞定

---
+++ WebContent/js/addRole.js
@@ -0,0 +1,28 @@
+window.onload = initHtml;
+
+function initHtml () {
+	var $$ = Dom7;
+	$$('#submit').on('click', function () {
+		var name = $$('#name').val();
+		var desc = $$('#message').val();
+
+		//遍历所有check
+	    var funcStr = ""; 
+        $$('input[type="checkbox"]:checked').each(
+            function() {
+                funcStr=funcStr+"-"+$$(this).prop('id');   
+            }
+        );
+
+        if (cheackIt) {
+    	    $$.post('addFunc', {name: name,desc:desc,}, function (data) {
+                
+            });   
+        };
+
+	});
+}
+
+function cheackIt () {
+
+}
