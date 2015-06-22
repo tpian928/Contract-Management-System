b5e38a00f510facbe689c69e1dd1eab8bd44438e
王怡飞 <TPIAN@wangyifeideMacBook-Air.local>
2015-06-22 19:34:27 +0800

    存档

---
+++ WebContent/js/index.js
@@ -0,0 +1,17 @@
+window.onload = initHtml;
+
+function initHtml () {
+	
+	var $$ = Dom7;
+
+    //点击分配按钮
+    $$(document).on('click','a', function () {
+        var cid = $$(this).prop('id');
+        var cname = $$('strong').html();
+
+        location.href="/CM/ac?cid="+cid+"&cname="+cname;
+
+    });
+
+}
+
