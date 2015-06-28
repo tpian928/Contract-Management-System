f569d3d56ecfb6c1c88777bcbec2b2317554701e
王怡飞 <TPIAN@wangyifeideMacBook-Air.local>
2015-06-28 18:26:20 +0800

    最终完成版

--- src/sever/LoginServlet.java
+++ src/sever/LoginServlet.java
@@ -1,6 +1,7 @@
 package sever;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -34,29 +35,35 @@ public class LoginServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		String name = request.getParameter("name");
 		String password = request.getParameter("pw");
-		
-		User m = UserJDBCAction.isMyUser(name, password);
-		JSONObject jsonObject = new JSONObject();
-		if (Func.isEmpty(m.getId())==false) {
-			request.getSession().setAttribute("access_taken", m.getAccess_taken());
-			request.getSession().setAttribute("id", m.getId());
-			try {
-				jsonObject.put("result", true);
-			} catch (JSONException e) {
-				e.printStackTrace();
+		User m = new User();
+		try {
+			m= UserJDBCAction.isMyUser(name, password);
+			JSONObject jsonObject = new JSONObject();
+			if (Func.isEmpty(m.getId())==false) {
+				request.getSession().setAttribute("access_taken", m.getAccess_taken());
+				request.getSession().setAttribute("id", m.getId());
+				try {
+					jsonObject.put("result", true);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
 			}
+			else {
+				try {
+					jsonObject.put("result", false);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}			
+			}
+			
+			//返回
+		    response.getOutputStream().write(jsonObject.toString().getBytes("UTF-8"));  
+		    response.setContentType("text/json; charset=UTF-8"); 			
+		} catch (Exception e) {
+		    PrintWriter out = response.getWriter();
+		    out.println("wrong is "+e+"\n user is "+m.getAccess_taken()+"  "+m.getName());
 		}
-		else {
-			try {
-				jsonObject.put("result", false);
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}			
-		}
-		
-		//返回
-	    response.getOutputStream().write(jsonObject.toString().getBytes("UTF-8"));  
-	    response.setContentType("text/json; charset=UTF-8"); 
+
 		
 	}
 
